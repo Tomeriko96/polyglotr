af2ffc9d5f87152207dd488188b6fefe41110363
Tomeriko96 <iwantomer@hotmail.com>
2024-01-26 10:32:57 +0100

    update: roxygen comments for wmcloud function

--- R/wmcloud_translate.R
+++ R/wmcloud_translate.R
@@ -1,17 +1,45 @@
-#' Translate text using WMCloud
+#' Translate content using WMCloud
 #'
 #' This function sends a POST request to the WMCloud translation API with the specified parameters,
-#' parses the JSON response, and returns the translated text.
+#' parses the JSON response, and returns the translated content.
 #'
-#' @param text The text to translate.
+#' @param content The content to translate. Can be plain text, a URL (for a webpage), a JSON string, or a Markdown string.
 #' @param target_language The target language for the translation (default is "en").
-#' @param source_language The source language of the text (default is "en").
+#' @param source_language The source language of the content (default is "en").
 #' @param format The format of the content ("html", "json", "markdown", "text", "svg", "webpage").
 #' @param model The model to use for the translation (only "nllb200-600M" is currently known to work).
 #'
-#' @return The translated text.
+#' @return The translated content.
+#'
+#' @examples
+#' \dontrun{
+#' # Translate plain text
+#' wmcloud_translate("rijst",
+#' target_language = "es",
+#' source_language = "nl", format = "text")
+#'
+#' # Translate a webpage
+#' wmcloud_translate("https://en.m.wikivoyage.org/wiki/Goes",
+#' target_language = "es",
+#' source_language = "en", format = "webpage")
+#'
+#' # Translate JSON content
+#' wmcloud_translate('{
+#'     "id": 1,
+#'     "title": "Chicken Biryani",
+#'     "description": "Chicken Biryani is a savory chicken and rice dish",
+#'     "ingredients": [ "Vegetable oil", "Garlic", "Ginger" ,"Rice"]
+#' }
+#'                ', target_language = "es", source_language = "en", format = "json")
+#'
+#' # Translate Markdown content
+#' wmcloud_translate('# Heading
+#'
+#' This is a [link to Wikipedia](https://wikipedia.org)
+#'                 ', target_language = "es", source_language = "en", format = "markdown")
+#' }
 #' @export
-wmcloud_translate <- function(text,
+wmcloud_translate <- function(content,
                               target_language = "en",
                               source_language = "en",
                               format = "text",
@@ -39,7 +67,7 @@ wmcloud_translate <- function(text,
     target_language = target_language,
     format = format,
     model = model,
-    content = text
+    content = content
   )
 
   # Convert the list to a JSON string
@@ -59,11 +87,8 @@ wmcloud_translate <- function(text,
   # Parse the response
   result <- httr::content(response, "parsed")
 
-  # Extract the translated text from the response
+  # Extract the translated content from the response
   translation <- result$translation
 
   return(translation)
 }
-
-
-
