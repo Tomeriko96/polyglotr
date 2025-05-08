7508087ffd5128bd5aac842a3d07ea8308ba46c1
Aslam Tanjung <a.hanif.tanjung@gmail.com>
2025-05-08 10:27:28 +0200

    docs: update roxygen with example with url

--- R/google_translate.R
+++ R/google_translate.R
@@ -1,20 +1,28 @@
-#' Translate text using google translate
+#' Translate text using Google Translate
 #'
-#' @param text This is the text that you want to translate.
+#' Translates input text to a specified language using the Google Translate mobile web interface.
+#' Automatically detects and preserves URLs by temporarily replacing them with placeholders.
+#'
+#' @param text This is the text that you want to translate. Can be a single string or a vector of strings.
 #' @param target_language This is the language that you want to translate the text into.
-#' The default value for this argument is "en" for English.
-#' @param source_language This is the language of the text that you want to translate.
-#' The default value for this argument is "auto",
-#' which means that the function will try to automatically detect the language of the text.
+#' The default value is "en" (English).
+#' @param source_language This is the language of the text to be translated.
+#' The default value is "auto", which attempts automatic language detection.
 #'
-#' @return Translated text.
+#' @return A translated string or vector of translated strings, matching the length of the input.
 #' @export
 #'
 #' @examples
 #' \donttest{
+#' # Translate a simple sentence
 #' google_translate("I love languages", target_language = "es")
+#'
+#' # Translate a vector of words
 #' text_to_translate <- c("the", "quick", "brown")
 #' google_translate(text_to_translate, "fr", "en")
+#'
+#' # Translate text containing a URL
+#' google_translate("Visit http://example.com for more info.", target_language = "de")
 #' }
 google_translate <- function(text, target_language = "en", source_language = "auto") {
   ## Validate target language code
