46dd20d33c4d6c271b81a14c48819097a1a5e551
tin900 <113692500+tin900@users.noreply.github.com>
2025-07-21 08:22:50 +0200

    Return language pairs as tibble in Apertium function
    Changed apertium_get_language_pairs to return a tibble of language pairs instead of raw response data. Updated _pkgdown.yml to include apertium_get_language_pairs in documentation.

--- R/apertium_get_language_pairs.R
+++ R/apertium_get_language_pairs.R
@@ -11,16 +11,18 @@
 #' \donttest{
 #' pairs <- apertium_get_language_pairs()
 #' head(pairs, 5)
-#' 
+#'
 #' # Using custom host
 #' pairs <- apertium_get_language_pairs(host = "https://your-custom-apertium-server.com/apy")
 #' }
 apertium_get_language_pairs <- function(host = "https://apertium.org/apy") {
-  
+
   formatted_link <- paste0(host, "/listPairs")
-  
+
   response <- httr::GET(formatted_link) %>%
     httr::content()
-  
-  return(response)
+
+  df_pairs <- purrr::map_dfr(response$responseData, tibble::as_tibble)
+
+  return(df_pairs)
 }
